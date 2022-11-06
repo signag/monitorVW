3199e77c14b7feb0188f407ff0223a3bd5b4f130
signag <siegfried.nagel@t-online.de>
2022-11-06 02:12:48 +0100

    Initial

---
+++ monitorVW/monitorVW.py
@@ -0,0 +1,488 @@
+#!/usr/bin/python3
+"""
+Module monitorVW
+
+This module periodically reads data from Fritz!Box Home Automation modules
+and and stores related measurement date in an InfluxDB
+"""
+
+import time
+import datetime
+import math
+import os.path
+import json
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+from weconnect.NativeAPI import WeConnect
+from weconnect.NativeAPI import VWError
+
+# Set up logging
+import logging
+from logging.config import dictConfig
+import logging_plus
+logger = logging_plus.getLogger("main")
+
+testRun = False
+servRun = False
+
+# Configuration defaults
+cfgFile = ""
+cfg = {
+    "measurementInterval": 120,
+    "weconUsername" : None,
+    "weconPassword" : None,
+    "weconSPin" : None,
+    "weconCarId" : None,
+    "InfluxOutput" : False,
+    "InfluxURL" : None,
+    "InfluxOrg" : None,
+    "InfluxToken" : None,
+    "InfluxBucket" : None,
+    "csvOutput" : False,
+    "csvFile" : "",
+    "carData" : []
+}
+
+# Constants
+CFGFILENAME = "monitorVW.json"
+
+def getCl():
+    """
+    getCL: Get and process command line parameters
+    """
+
+    import argparse
+    import os.path
+
+    global logger
+    global testRun
+    global servRun
+    global cfgFile
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=
+    """
+    This program periodically reads data from VW WeConnect
+    and stores these as measurements in an InfluxDB database.
+
+    If not otherwises specified on the command line, a configuration file
+       monitorVW.json
+    will be searched sequentially under ./tests/data, $HOME/.config or /etc.
+
+    This configuration file specifies credentials for WeConnect access,
+    the car data to read, the connection to the InfluxDB and other runtime parameters.
+    """
+    )
+    parser.add_argument("-t", "--test", action = "store_true", help="Test run - single cycle - no wait")
+    parser.add_argument("-s", "--service", action = "store_true", help="Run as service - special logging")
+    parser.add_argument("-l", "--log", action = "store_true", help="Shallow (module) logging")
+    parser.add_argument("-L", "--Log", action = "store_true", help="Deep logging")
+    parser.add_argument("-F", "--Full", action = "store_true", help="Full logging")
+    parser.add_argument("-f", "--file", help="Logging configuration from specified JSON dictionary file")
+    parser.add_argument("-v", "--verbose", action = "store_true", help="Verbose - log INFO level")
+    parser.add_argument("-c", "--config", help="Path to config file to be used")
+
+    args = parser.parse_args()
+
+    # Disable logging
+    logger = logging_plus.getLogger("main")
+    logger.addHandler(logging.NullHandler())
+    fLogger = logging_plus.getLogger(WeConnect.__module__)
+    fLogger.addHandler(logging.NullHandler())
+    rLogger = logging_plus.getLogger()
+    rLogger.addHandler(logging.NullHandler())
+
+    # Set handler and formatter to be used
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+    formatter2 = logging.Formatter('%(asctime)s %(name)-33s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+
+    if args.log:
+        # Shallow logging
+        handler.setFormatter(formatter2)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+
+    if args.Log:
+        # Deep logging
+        handler.setFormatter(formatter2)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+        fLogger.addHandler(handler)
+        fLogger.setLevel(logging.DEBUG)
+
+    if args.Full:
+        # Full logging
+        handler.setFormatter(formatter2)
+        rLogger.addHandler(handler)
+        rLogger.setLevel(logging.DEBUG)
+        # Activate logging of function entry and exit
+        logging_plus.registerAutoLogEntryExit()
+
+    if args.file:
+        # Logging configuration from file
+        logDictFile = args.file
+        if not os.path.exists(logDictFile):
+            raise ValueError("Logging dictionary file from command line does not exist: " + logDictFile)
+
+        # Load dictionary
+        with open(logDictFile, 'r') as f:
+            logDict = json.load(f)
+
+        # Set config file for logging
+        dictConfig(logDict)
+        logger = logging.getLogger()
+        # Activate logging of function entry and exit
+        #logging_plus.registerAutoLogEntryExit()
+
+    # Explicitly log entry
+    if args.Log or args.Full:
+        logger.logEntry("getCL")
+    if args.log:
+        logger.debug("Shallow logging (main only)")
+    if args.Log:
+        logger.debug("Deep logging")
+    if args.file:
+        logger.debug("Logging dictionary from %s", logDictFile)
+
+    if args.verbose or args.service:
+        if not args.log and not args.Log and not args.Full:
+            logger.addHandler(handler)
+            logger.setLevel(logging.INFO)
+            fLogger.addHandler(handler)
+            fLogger.setLevel(logging.WARNING)
+
+    if args.test:
+        testRun = True
+
+    if args.service:
+        servRun = True
+
+    if testRun:    
+        logger.debug("Test run mode activated")
+    else:
+        logger.debug("Test run mode deactivated")
+        
+    if servRun:    
+        logger.debug("Service run mode activated")
+    else:
+        logger.debug("Service run mode deactivated")
+
+    if args.config:
+        cfgFile = args.config
+        logger.debug("Config file: %s", cfgFile)
+    else:
+        logger.debug("No Config file specified on command line")
+
+    if args.Log or args.Full:
+        logger.logExit("getCL")
+
+def getConfig():
+    """
+    Get configuration for fritzToInfluxHA
+    """
+    global cfgFile
+    global cfg
+    global logger
+
+    # Check config file from command line
+    if cfgFile != "":
+        if not os.path.exists(cfgFile):
+            raise ValueError("Configuration file from command line does not exist: ", cfgFile)
+        logger.info("Using cfgFile from command line: %s", cfgFile)
+
+    if cfgFile == "":
+        # Check for config file in ./tests/data directory
+        curDir = os.path.dirname(os.path.realpath(__file__))
+        curDir = os.path.dirname(curDir)
+        cfgFile = curDir + "/tests/data/" + CFGFILENAME
+        if not os.path.exists(cfgFile):
+            # Check for config file in /etc directory
+            logger.info("Config file not found: %s", cfgFile)
+            cfgFile = ""
+
+    if cfgFile == "":
+        # Check for config file in home directory
+        homeDir = os.environ['HOME']
+        cfgFile = homeDir + "/.config/" + CFGFILENAME
+        if not os.path.exists(cfgFile):
+            # Check for config file in /etc directory
+            logger.info("Config file not found: %s", cfgFile)
+            cfgFile = "/etc/" + CFGFILENAME
+            if not os.path.exists(cfgFile):
+                logger.info("Config file not found: %s", cfgFile)
+                cfgFile = ""
+
+    if cfgFile == "":
+        # No cfg available 
+        logger.info("No config file available. Using default configuration")
+    else:
+        logger.info("Using cfgFile: %s", cfgFile)
+        with open(cfgFile, 'r') as f:
+            conf = json.load(f)
+            if "measurementInterval" in conf:
+                cfg["measurementInterval"] = conf["measurementInterval"]
+            if "weconUsername" in conf:
+                cfg["weconUsername"] = conf["weconUsername"]
+            if "weconPassword" in conf:
+                cfg["weconPassword"] = conf["weconPassword"]
+            if "weconSPin" in conf:
+                cfg["weconSPin"] = conf["weconSPin"]
+            if "weconCarId" in conf:
+                cfg["weconCarId"] = conf["weconCarId"]
+            if "InfluxOutput" in conf:
+                cfg["InfluxOutput"] = conf["InfluxOutput"]
+            if "InfluxURL" in conf:
+                cfg["InfluxURL"] = conf["InfluxURL"]
+            if "InfluxOrg" in conf:
+                cfg["InfluxOrg"] = conf["InfluxOrg"]
+            if "InfluxToken" in conf:
+                cfg["InfluxToken"] = conf["InfluxToken"]
+            if "InfluxBucket" in conf:
+                cfg["InfluxBucket"] = conf["InfluxBucket"]
+            if "csvOutput" in conf:
+                cfg["csvOutput"] = conf["csvOutput"]
+            if "csvFile" in conf:
+                cfg["csvFile"] = conf["csvFile"]
+            if cfg["csvFile"] == "":
+                cfg["csvOutput"] = False
+            if "carData" in conf:
+                cfg["carData"] = conf["carData"]
+                
+    # Check WeConnect credentials
+    if not cfg["weconUsername"]:
+        raise ValueError("weconUsername not specified")
+    if not cfg["weconPassword"]:
+        raise ValueError("weconPassword not specified")
+    if not cfg["weconSPin"]:
+        raise ValueError("weconSPin not specified")
+    if not cfg["weconCarId"]:
+        raise ValueError("weconCarId not specified")
+    if (isinstance(cfg["weconSPin"], int)):
+        cfg["weconSPin"] = str(cfg["weconSPin"]).zfill(4)
+    if (isinstance(cfg["weconSPin"], str)):
+        if (len(cfg["weconSPin"]) != 4):
+            raise ValueError('Wrong S-PIN format: must be 4-digits')
+        try:
+            d = int(cfg["weconSPin"])
+        except ValueError:
+            raise ValueError('Wrong S-PIN format: must be 4-digits')
+    else:
+        raise ValueError('Wrong S-PIN format: must be 4-digits')
+
+    logger.info("Configuration:")
+    logger.info("    measurementInterval:%s", cfg["measurementInterval"])
+    logger.info("    weconUsername:%s", cfg["weconUsername"])
+    logger.info("    weconPassword:%s", cfg["weconPassword"])
+    logger.info("    weconSPin:%s", cfg["weconSPin"])
+    logger.info("    weconCarId:%s", cfg["weconCarId"])
+    logger.info("    InfluxOutput:%s", cfg["InfluxOutput"])
+    logger.info("    InfluxURL:%s", cfg["InfluxURL"])
+    logger.info("    InfluxOrg:%s", cfg["InfluxOrg"])
+    logger.info("    InfluxToken:%s", cfg["InfluxToken"])
+    logger.info("    InfluxBucket:%s", cfg["InfluxBucket"])
+    logger.info("    csvOutput:%s", cfg["csvOutput"])
+    logger.info("    csvFile:%s", cfg["csvFile"])
+    logger.info("    carData:%s", len(cfg["carData"]))
+    for ind in range(0, len(cfg["carData"])):
+        dev = cfg["carData"][ind]
+        # logger.info("       %s (%s - %s)", dev["ain"], dev["location"], dev["sublocation"])
+
+
+def waitForNextCycle():
+    """
+    Wait for next measurement cycle.
+
+    This function assures that measurements are done at specific times depending on the specified interval
+    In case that measurementInterval is an integer multiple of 60, the waiting time is calculated in a way,
+    that one measurement is done every full hour.
+    """
+    global cfg
+
+    if (cfg["measurementInterval"] % 60 == 0)\
+    or (cfg["measurementInterval"] % 120 == 0)\
+    or (cfg["measurementInterval"] % 240 == 0)\
+    or (cfg["measurementInterval"] % 300 == 0)\
+    or (cfg["measurementInterval"] % 360 == 0)\
+    or (cfg["measurementInterval"] % 600 == 0)\
+    or (cfg["measurementInterval"] % 720 == 0)\
+    or (cfg["measurementInterval"] % 900 == 0)\
+    or (cfg["measurementInterval"] % 1200 == 0)\
+    or (cfg["measurementInterval"] % 1800 == 0):
+        tNow = datetime.datetime.now()
+        seconds = 60 * tNow.minute
+        period = math.floor(seconds/cfg["measurementInterval"])
+        waitTimeSec = (period + 1) * cfg["measurementInterval"] - (60 * tNow.minute + tNow.second + tNow.microsecond / 1000000)
+        logger.debug("At %s waiting for %s sec.", datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S,"), waitTimeSec)
+        time.sleep(waitTimeSec)
+    elif (cfg["measurementInterval"] % 2 == 0)\
+      or (cfg["measurementInterval"] % 4 == 0)\
+      or (cfg["measurementInterval"] % 5 == 0)\
+      or (cfg["measurementInterval"] % 6 == 0)\
+      or (cfg["measurementInterval"] % 10 == 0)\
+      or (cfg["measurementInterval"] % 12 == 0)\
+      or (cfg["measurementInterval"] % 15 == 0)\
+      or (cfg["measurementInterval"] % 20 == 0)\
+      or (cfg["measurementInterval"] % 30 == 0):
+            tNow = datetime.datetime.now()
+            seconds = 60 * tNow.minute + tNow.second
+            period = math.floor(seconds/cfg["measurementInterval"])
+            waitTimeSec = (period + 1) * cfg["measurementInterval"] - seconds
+            logger.debug("At %s waiting for %s sec.", datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S,"), waitTimeSec)
+            time.sleep(waitTimeSec)
+    else:
+        waitTimeSec =cfg["measurementInterval"]
+        logger.debug("At %s waiting for %s sec.", datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S,"), waitTimeSec)
+        time.sleep(waitTimeSec)
+
+#============================================================================================
+# Start __main__
+#============================================================================================
+#
+# Get Command line options
+getCl()
+
+logger.info("=============================================================")
+logger.info("monitorVW started")
+logger.info("=============================================================")
+
+# Get configuration
+getConfig()
+
+fb = None
+influxClient = None
+influxWriteAPI = None
+
+try:
+    # Log in to WeConnect
+    userName = cfg["weconUsername"]
+    password = cfg["weconPassword"]
+    pin = cfg["weconSPin"]
+    vwc = WeConnect(userName, password, pin)
+    logger.debug("WeConnect vwc instantiated")
+    vwc.login()
+    logger.debug("WeConnect login successful")
+    
+    # Get car to query
+    theCar = None
+    logger.debug("Searching car in registered cars")
+    cars = vwc.get_real_car_data()
+    if (cars and len(cars)):
+        for car in cars.get('realCars',[]):
+            vin = car.get('vehicleIdentificationNumber','UNKNOWN')
+            logger.debug("Found %s", vin)
+            if vin == cfg["weconCarId"]:
+                theCar = car
+    else:
+        raise VWError("No cars registered at WeConnect for specified profile")
+    
+    if theCar:
+        theVin = theCar.get('vehicleIdentificationNumber','UNKNOWN')
+        theDetails = vwc.get_vehicle_data(theVin)
+        theCardata = theDetails.get('vehicleDataDetail',[]).get('carportData',[])
+        theVsr = vwc.get_vsr(theVin)
+        thePvsr = vwc.parse_vsr(theVsr)
+        theStatus = thePvsr.get('status',[])
+    else:
+        raise VWError("Requested car not registered at WeConnect")
+    
+    # Instatntiate InfluxDB access
+    if cfg["InfluxOutput"]:
+        influxClient = influxdb_client.InfluxDBClient(
+            url=cfg["InfluxURL"],
+            token=cfg["InfluxToken"],
+            org=cfg["InfluxOrg"]
+        )
+        influxWriteAPI = influxClient.write_api(write_options=SYNCHRONOUS)
+        logger.debug("Influx interface instantiated")
+
+    noWait = False
+    stop = False
+
+except VWError as error:
+    logger.critical("Unexpected error: %s", error.message)
+    stop = True
+    vwc = None
+    influxClient = None
+    influxWriteAPI = None
+
+failcount = 0
+while not stop:
+    try:
+        # Wait unless noWait is set in case of sensor error.
+        # Akip waiting for test run
+        if not noWait and not testRun:
+            waitForNextCycle()
+        noWait = False
+
+        # Query car data
+        mileage = theStatus.get('distance_covered','UNKNOWN')
+        # fb.evaluateDeviceInfo()
+        if not servRun:
+            logger.info("Measurement completed")
+
+        # Write data to CSV
+        #if cfg["csvOutput"]:
+        #    fp = cfg["csvFile"]
+        #    fb.writeDataToCsv(fp)
+
+        # Write data to InfluxDB
+        # if cfg["InfluxOutput"]:
+        #    fb.writeDataToInflux(influxWriteAPI, cfg["InfluxOrg"], cfg["InfluxBucket"])
+        #    if not servRun:
+        #        logger.info("Data written to InfluxDB")
+
+        if testRun:
+            # Stop in case of test run
+            stop = True
+
+#    except FritzBoxIgnoreableError as error:
+#        failcount = failcount + 1
+#        logger.error("Ignored FritzBoxIgnoreableError (%s): %s", failcount, error.message)
+#        noWait = True
+#        if testRun:
+#            # Stop in case of test run
+#            stop = True
+#        else:
+#            time.sleep(2.0)
+#            continue
+#
+    except VWError as error:
+        logger.critical("Unexpected error: %s", error.message)
+        if vwc:
+            del vwc
+        if influxClient:
+            del influxClient
+        if influxWriteAPI:
+            del influxWriteAPI
+
+    except Exception as e:
+        logger.critical("Unexpected error (%s): %s", e.__class__, e.__cause__)
+        if vwc:
+            del vwc
+        if influxClient:
+            del influxClient
+        if influxWriteAPI:
+            del influxWriteAPI
+        raise error
+
+    except KeyboardInterrupt:
+        if vwc:
+            del vwc
+        if influxClient:
+            del influxClient
+        if influxWriteAPI:
+            del influxWriteAPI
+
+if vwc:
+    del vwc
+if influxClient:
+    del influxClient
+if influxWriteAPI:
+    del influxWriteAPI
+
+logger.info("=============================================================")
+logger.info("monitorVW terminated")
+logger.info("=============================================================")
